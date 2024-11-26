a51e81495450952c8ac0f4af30a48bcaaa7d99a6
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-26 02:49:34 -0600

    refactor Home component layout for improved responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import { JsonToXmlConverter } from "@/components/json-to-xml-converter";
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">JSON to XML Converter</h1>
+    <main className="min-h-[calc(100vh-4rem)]">
       <JsonToXmlConverter />
     </main>
   );
